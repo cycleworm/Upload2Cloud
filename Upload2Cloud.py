c10ea4610c10d9b9bb2177129677fc03e52ba9b9
Andreas Kreiseder <kreiseder.a@gmail.com>
2020-12-22 23:47:07 +0100

    change fancylink button to checkbox, add state to json

--- Upload2Cloud.py
+++ Upload2Cloud.py
@@ -6,16 +6,21 @@ import webbrowser
 import threading
 import json
 import siaskynet as skynet
-
+import time
 
 def upload():
     client = skynet.SkynetClient()
     upload.skylink = client.upload_file(str(sys.argv[1]))
     setSkylink(upload.skylink.replace("sia://", "https://"+variable.get()+"/"))
+
+    if(fancyState.get()==True):
+        threadCreateFancyLink()
+
     btnOpenLink['state'] = tk.NORMAL
     btnCopyLink['state'] = tk.NORMAL
     btnFancyLink['state'] = tk.NORMAL
     opt['state'] = tk.NORMAL
+    chkFancyLink['state'] = tk.NORMAL
 
 
 def openLink():
@@ -28,18 +33,39 @@ def copyLink():
     pyperclip.copy(upload.skylink.replace("sia://", "https://"+variable.get()+"/"))
 
 
+def threadCreateFancyLink():
+    threading.Thread(target=createFancyLink).start()
+
+
 def createFancyLink():
+    btnOpenLink['state'] = tk.DISABLED
+    btnCopyLink['state'] = tk.DISABLED
+    btnFancyLink['state'] = tk.DISABLED
+    opt['state'] = tk.DISABLED
+    chkFancyLink['state'] = tk.DISABLED
+
+    setSkylink("creating fancy link...")
+
+    with open(configFilePath, "r") as json_data_file:
+        data = json.load(json_data_file)
+
+    data["createFancyLink"] = fancyState.get()
+
+    with open(configFilePath, "w") as jsonFile:
+        json.dump(data, jsonFile)
+    json_data_file.close()
+
     file_stats = os.stat(sys.argv[1])
     file_size = sizeof_fmt(file_stats.st_size)
     filename = os.path.basename(sys.argv[1])
-    skylink = upload.skylink.replace("sia://", "https://" + variable.get() + "/")
+    upload.skylink = upload.skylink.replace("sia://", "https://" + variable.get() + "/")
 
     f = open(web_template_path+"Webtemplate/original_index.html", 'r')
     web_template = f.read()
     f.close()
     web_template = web_template.replace('__filename__', filename)
     web_template = web_template.replace('__filesize__', file_size)
-    web_template = web_template.replace('__skylink__', skylink)
+    web_template = web_template.replace('__skylink__', upload.skylink)
 
     f = open(web_template_path+"Webtemplate/index.html", 'w')
     f.write(web_template)
@@ -48,6 +74,13 @@ def createFancyLink():
     client = skynet.SkynetClient()
     upload.skylink = client.upload_directory(web_template_path+"Webtemplate")
     setSkylink(upload.skylink.replace("sia://", "https://"+variable.get()+"/"))
+
+    btnOpenLink['state'] = tk.NORMAL
+    btnCopyLink['state'] = tk.NORMAL
+    btnFancyLink['state'] = tk.NORMAL
+    opt['state'] = tk.NORMAL
+    chkFancyLink['state'] = tk.NORMAL
+
 
 def setSkylink(text):
     skylinkEntry.delete(0, "end")
@@ -69,6 +102,11 @@ def readActivePortals(filename):
     return activePortalList
 
 
+def initFancyState(filename):
+    with open(filename) as json_data_file:
+        data = json.load(json_data_file)
+    fancyState.set(data["createFancyLink"])
+
 
 def callbackDropdown(*args):
     setSkylink("https://{}".format(variable.get())+upload.skylink.replace("sia://", "/"))
@@ -77,6 +115,8 @@ def callbackDropdown(*args):
     with open(configFilePath, 'w') as json_data_file:
         json.dump(portalURL, json_data_file)
     json_data_file.close()
+
+
 
 
 def sizeof_fmt(num, suffix='B'):
@@ -110,7 +150,7 @@ activePortalList = readActivePortals(configFilePath)
 # Configure Window
 icon_path = os.path.join(application_path, "upload.ico")
 root = tk.Tk()
-root.title("U2C - Ver. 1.0.2")
+root.title("U2C - Ver. 1.0.3")
 root.minsize(1000, 60)
 root.iconbitmap(icon_path)
 root.resizable(0, 0)
@@ -144,8 +184,13 @@ btnOpenLink = tk.Button(root, text="Open Link", state=tk.DISABLED, command=openL
 btnOpenLink.pack(padx=0, side="left")
 btnCopyLink = tk.Button(root, text="Copy Link", state=tk.DISABLED, command=copyLink)
 btnCopyLink.pack(padx=10, side="left")
-btnFancyLink = tk.Button(root, text="Create Fancy Link", state=tk.DISABLED, command=createFancyLink)
+btnFancyLink = tk.Button(root, text="Create Fancy Link", state=tk.DISABLED, command=threadCreateFancyLink)
 btnFancyLink.pack(padx=0, side="left")
+fancyState = tk.BooleanVar()
+chkFancyLink = tk.Checkbutton(root, text="Fancy Link", variable=fancyState, state=tk.DISABLED, command=threadCreateFancyLink)
+chkFancyLink.pack(padx=10, side="left")
+
+initFancyState(configFilePath)
 
 # Update Tkinter to get actual window size
 root.update()
